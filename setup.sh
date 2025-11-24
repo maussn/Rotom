f663dfc9106e6b2b94d8968be058258f74a0628e
maussn <naber.maurits@gmail.com>
2025-11-24 15:23:58 +0100

    move setup.sh to root directory

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 apt update
 
 if ! command -v mysql >/dev/null 2>&1
-then ./install-mysql.sh
+then (./rotom.bash/setup/install-mysql.sh)
 fi
 
 if ! command -v npm >/dev/null 2>&1
-then ./install-nodejs.sh
+then (./rotom.bash/setup/install-nodejs.sh)
 fi
 
 if ! command -v sdk >/dev/null 2>&1
-then ./install-scala.sh
+then (./rotom.bash/setup/install-scala.sh)
 fi
 
