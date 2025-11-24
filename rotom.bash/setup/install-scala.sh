3eeb593e1958de6a210521b89338a2fad8367320
maussn <naber.maurits@gmail.com>
2025-11-24 12:29:18 +0100

    correctly call function in setup.sh

--- rotom.bash/setup/install-scala.sh
+++ rotom.bash/setup/install-scala.sh
@@ -13,11 +13,11 @@ if ! command -v java >/dev/null 2>&1; then
 fi
 
 if ! command -v scala >/dev/null 2>&1; then
-  install_sdkman()
+  install_sdkman
   sdk install scala
 fi
 
 if ! command -v sbt >/dev/null 2>&1; then
-  install_sdkman()
+  install_sdkman
   sdk install sbt
 fi
