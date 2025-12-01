6b7a0063d036cea8dafee07f10ba698a6e8da42a
maussn <naber.maurits@gmail.com>
2025-12-01 11:10:06 +0100

    rewrite queries

--- rotom.persistence/src/main/scala/nl/sogyo/persistence/AbstractDatabases.scala
+++ rotom.persistence/src/main/scala/nl/sogyo/persistence/AbstractDatabases.scala
@@ -5,6 +5,7 @@ import slick.lifted.TableQuery
 
 import scala.concurrent.Await
 import scala.concurrent.duration.*
+import java.util.UUID
 
 trait ProfileProvider {
   val profile: slick.jdbc.JdbcProfile
@@ -39,12 +40,41 @@ trait DatabaseReader {
   import profile.api.* 
 
   def queryAllItems(): Seq[Item] =
-    def setupQuery() =
-      itemsDatabase.table
-    itemsDatabase.exec(setupQuery().result)
+    val query = itemsDatabase.table
+    itemsDatabase.exec(query.result)
 
   def queryAccountByUsername(username: String): Option[Account] =
-    def setupQuery(username: String) = 
-      accountsDatabase.table.filter(_.username === username)
-    accountsDatabase.exec(setupQuery(username).result).headOption
+    val query = accountsDatabase.table.filter(_.username === username)
+    val result = accountsDatabase.exec(query.result)
+    if result.size > 1 then throw NonUniqueUsernameException(username)
+    result.headOption
+
+  def queryItemById(itemId: UUID): Option[Item] = 
+    val query = itemsDatabase.table.filter(_.id === itemId)
+    val result = itemsDatabase.exec(query.result)
+    if result.size > 1 then throw NonUniqueUUIDException(itemId)
+    result.headOption
+
+  // def queryAvailableItemById(itemId: UUID): Option[Item] =
+  //   val query = 
 }
+
+class MultipleEntriesException(
+  private val message: String,
+  private val cause: Throwable = None.orNull
+) extends Exception(message, cause)
+
+case class NonUniqueUsernameException(
+  private val username: String,
+  private val cause: Throwable = None.orNull
+) extends MultipleEntriesException(
+  s"Multiple accounts assigned to same username=$username. This should not be thrown.",
+  cause
+)
+
+case class NonUniqueUUIDException(
+  private val uuid: UUID,
+  private val cause: Throwable = None.orNull
+) extends MultipleEntriesException(
+  s"Found multiple entries for uuid=$uuid. This should not be thrown.", cause
+)
